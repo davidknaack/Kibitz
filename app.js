3746c47f4333e38d17f73efb2f3cf3aebdd08242
OpenShift guest <davidknaack@gmail.com>
2013-06-26 11:33:16 -0400

    update to socket.io

--- app.js
+++ app.js
@@ -1,39 +1,57 @@
 var express = require('express');
 var app = module.exports = express();
 var config = require('./config.js')(app, express);
+var jade = require('jade');
+var fs = require('fs');
 
-var dataModels = {  
-    kibitz: { 
-        kibItems: [], 
-        add: function(kib){ 
-            dataModels.kibitz.kibItems.push(kib);
-            var l = dataModels.kibitz.kibItems.length;
-            if ( l > 5)
-                dataModels.kibitz.kibItems = dataModels.kibitz.kibItems.slice(l-5,l);
-        }}
-};
+var kibitzDM = function(){  
+    var key = 'kibitzs.jade';
+    var path = __dirname + '/views/'+key;
+    var str = jade.cache[key] || (jade.cache[key] = fs.readFileSync(path, 'utf8'));
+    var kibView = jade.compile(str);
+
+    var kibItems = [];
+    
+    var add = function(user,msg){ 
+        kibItems.push({date: 1, from: user, text: msg});
+        
+        var l = kibItems.length;
+        if (l > 5)
+            kibItems = kibItems.slice(l-5,l);
+    };
+    add( 'guest', 'first post');
+    
+    var getView = function(user){
+        return kibView({
+            username: user,
+            kibItems: kibItems
+        });
+    };
+    
+    return {
+        getView : getView,
+        kibItems : kibItems,
+        add : add
+    };
+}();
 
 app.get('/', function(req, res){
     req.session.username = req.session.username||'guest';
+    console.log( 'render : ' + kibitzDM.kibItems);
     res.render('index.jade', {
-        title: 'OMG Kibitz',
         username: req.session.username,
-        kibItems: dataModels.kibitz.kibItems
+        kibItems: kibitzDM.kibItems
     });
 });
 
 app.post('/posta', function(req, res){
-    console.log(req.body);
-
     req.session.username = req.body.from;
-    
     if (req.body.newtext)
-        dataModels.kibitz.add({date: 1, from: req.session.username, text: req.body.newtext});
-    
-    res.render('kibitzs.jade', {
-        kibItems: dataModels.kibitz.kibItems
-    });
+        kibitzDM.add(req.session.username, req.body.newtext);
+    io.sockets.emit('kibView', kibitzDM.getView(req.session.username));
+    res.send('');
 });
 
 console.log(process.env.PORT);
-app.listen(process.env.PORT);
+var io = require('socket.io').listen(app.listen(process.env.PORT));
+
